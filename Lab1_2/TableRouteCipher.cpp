31caeaa340249de6ae88c72fab65796333fb4dd7
daniilalatov <144266720+maxkol369@users.noreply.github.com>
2025-11-13 19:05:15 +0300

    Create TableRouteCipher.cpp

---
+++ Lab1_2/TableRouteCipher.cpp
@@ -0,0 +1,49 @@
+#include "TableRouteCipher.h"
+
+TableRouteCipher::TableRouteCipher(int skey) { key = skey; }
+
+string TableRouteCipher ::encrypt(string& text)
+{
+    int k = 0;
+    int simvoli = text.size();
+    int stroki = ((simvoli) / key);
+    char** tabl = new char*[stroki];
+    for(int i = 0; i < stroki; i++)
+        tabl[i] = new char[key];
+    for(int i = 0; i < stroki; i++)
+        for(int j = 0; j < key; j++) {
+            if(k < simvoli) {
+                tabl[i][j] = text[k];
+                k++;
+            }
+        }
+    k = 0;
+    for(int j = key - 1; j >= 0; j--)
+        for(int i = 0; i < stroki; i++) {
+            text[k] = tabl[i][j];
+            k++;
+        }
+    return text;
+}
+
+string TableRouteCipher ::decrypt(string& text)
+{
+    int k = 0;
+    int simvoli = text.size();
+    int stroki = ((simvoli) / key);
+    char** tabl = new char*[stroki];
+    for(int i = 0; i < stroki; i++)
+        tabl[i] = new char[key];
+    for(int j = key - 1; j >= 0; j--)
+        for(int i = 0; i < stroki; i++) {
+            tabl[i][j] = text[k];
+            k++;
+        }
+    k = 0;
+    for(int i = 0; i < stroki; i++)
+        for(int j = 0; j < key; j++) {
+            text[k] = tabl[i][j];
+            k++;
+        }
+    return text;
+}

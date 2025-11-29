83da4ec1c26b46199e2f6821858f7ce6ea54b22a
daniilalatov <144266720+maxkol369@users.noreply.github.com>
2025-11-29 11:34:17 +0300

    Update TableRouteCipher.cpp

--- Lab1_2/TableRouteCipher.cpp
+++ Lab1_2/TableRouteCipher.cpp
@@ -1,49 +1,92 @@
 #include "TableRouteCipher.h"
+#include <locale>
+#include <codecvt>
+#include <iostream>
 
-TableRouteCipher::TableRouteCipher(int skey) { key = skey; }
-
-string TableRouteCipher ::encrypt(string& text)
+TableRouteCipher::TableRouteCipher(int skey) { 
+    key = skey; 
+}
+string TableRouteCipher::encrypt(string& text)
 {
+    std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
+    std::wstring wtext = converter.from_bytes(text);
+    
     int k = 0;
-    int simvoli = text.size();
-    int stroki = ((simvoli) / key);
-    char** tabl = new char*[stroki];
+    int simvoli = wtext.size();
+    
+    if (simvoli % key != 0) {
+        throw cipher_error("Длина текста должна быть кратна ключу");
+    }  
+    int stroki = simvoli / key;
+    
+    wchar_t** tabl = new wchar_t*[stroki];
     for(int i = 0; i < stroki; i++)
-        tabl[i] = new char[key];
+        tabl[i] = new wchar_t[key];
+    
     for(int i = 0; i < stroki; i++)
         for(int j = 0; j < key; j++) {
             if(k < simvoli) {
-                tabl[i][j] = text[k];
+                tabl[i][j] = wtext[k];
                 k++;
             }
         }
+    
     k = 0;
+
     for(int j = key - 1; j >= 0; j--)
         for(int i = 0; i < stroki; i++) {
-            text[k] = tabl[i][j];
+            wtext[k] = tabl[i][j];
             k++;
         }
+    
+    text = converter.to_bytes(wtext);
+    
+    for(int i = 0; i < stroki; i++)
+        delete[] tabl[i];
+    delete[] tabl;
+    
     return text;
 }
 
-string TableRouteCipher ::decrypt(string& text)
+string TableRouteCipher::decrypt(string& text)
 {
+  
+    std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
+    std::wstring wtext = converter.from_bytes(text);
+    
     int k = 0;
-    int simvoli = text.size();
-    int stroki = ((simvoli) / key);
-    char** tabl = new char*[stroki];
+    int simvoli = wtext.size();
+    
+    if (simvoli % key != 0) {
+        throw cipher_error("Длина текста должна быть кратна ключу");
+    }
+    
+    int stroki = simvoli / key;
+
+    wchar_t** tabl = new wchar_t*[stroki];
     for(int i = 0; i < stroki; i++)
-        tabl[i] = new char[key];
+        tabl[i] = new wchar_t[key];
+
     for(int j = key - 1; j >= 0; j--)
         for(int i = 0; i < stroki; i++) {
-            tabl[i][j] = text[k];
-            k++;
+            if (k < simvoli) {
+                tabl[i][j] = wtext[k];
+                k++;
+            }
         }
+    
     k = 0;
+
     for(int i = 0; i < stroki; i++)
         for(int j = 0; j < key; j++) {
-            text[k] = tabl[i][j];
+            wtext[k] = tabl[i][j];
             k++;
         }
+    text = converter.to_bytes(wtext);
+    
+    for(int i = 0; i < stroki; i++)
+        delete[] tabl[i];
+    delete[] tabl;
+    
     return text;
 }
